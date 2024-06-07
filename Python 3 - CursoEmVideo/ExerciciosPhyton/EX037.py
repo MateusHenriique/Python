0ace8c85ba7706f6ebcef13eff22624991c39ebb
MateuSh5453 <mateush5453@gmail.com>
2024-06-07 20:24:27 -0300

    Finalizei as Condicionais.

--- Python 3 - CursoEmVideo/ExerciciosPhyton/EX037.py
+++ Python 3 - CursoEmVideo/ExerciciosPhyton/EX037.py
@@ -1,14 +1,20 @@
-inteiro = int(input('digite um numero inteiro: '))
+inteiro = int(input('Digite um numero inteiro: '))
 print('-='*20)
-print('conversor de inteiro')
+print('Conversor De Inteiro')
 print('-='*20)
-print('[ 1 ] converter para BINARIO')
-print('[ 2 ] converter para OCTAL')
-print('[ 3 ] converter para HEXADECIMAL')
+print('[ 1 ] Converter para BINARIO')
+print('[ 2 ] Converter para OCTAL')
+print('[ 3 ] Converter para HEXADECIMAL')
+
+import math 
 
 converte = int(input('Sua opção: '))
 if converte == 1:
-    
+    print('O numero {} convertido em binario fica: {}'.format(inteiro, bin(inteiro)[2:]))        
 elif converte == 2:
-
+    print('O numero {} convertodo em octal fica: {}'.format(inteiro, oct(inteiro)[2:]))
 elif converte == 3:
+    hexadecimal = hex(inteiro)
+    print('O numero {} convertido em hexadecimal fica: {}'.format(inteiro, hex(inteiro)[2:]))
+else:
+    print('Opção invalida tente novamente.')
