419cf104990d5ae732c1705bd441d38dae9968e2
MateuSh5453 <mateush5453@gmail.com>
2024-06-16 21:46:06 -0300

    Atualização EX045

--- Python 3 - CursoEmVideo/ExerciciosPhyton/EX045.py
+++ Python 3 - CursoEmVideo/ExerciciosPhyton/EX045.py
@@ -1,3 +1,14 @@
 print('-='*20)
-print({:^40})
+print('{:^40}'.format('JOGO PEDRA PAPEL E TESOURA'))
 print('-='*20)
+
+import random
+from time import sleep
+
+print('[ 1 ] Pedra')
+print('[ 2 ] Papel')
+print('[ 3 ] Tesoura')
+jogada_usuario = int(input('Qual será a sua escolha: '))
+print('-='*40)
+print('A maquina escolheu ...')
+sleep(5)
