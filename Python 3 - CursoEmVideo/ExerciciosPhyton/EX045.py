be410e89eae295ade0c997651742d898cdee06a1
MateuSh5453 <mateush5453@gmail.com>
2024-06-18 13:35:29 -0300

    Finalizei o exercício

--- Python 3 - CursoEmVideo/ExerciciosPhyton/EX045.py
+++ Python 3 - CursoEmVideo/ExerciciosPhyton/EX045.py
@@ -9,6 +9,28 @@ print('[ 1 ] Pedra')
 print('[ 2 ] Papel')
 print('[ 3 ] Tesoura')
 jogada_usuario = int(input('Qual será a sua escolha: '))
-print('-='*40)
-print('A maquina escolheu ...')
+print('-='*20)
+print('{:^40}'.format('Carregando...'))
+print('-='*20)
 sleep(5)
+jogada_maquina = random.randint(1, 3)
+if jogada_maquina == 1 and jogada_usuario == 2:
+    print('Usuario: PAPEL!   x   Maquina: PEDRA!')
+    print('Usuario VENCEU!')
+elif jogada_maquina == 2 and jogada_usuario == 3:
+    print('Usuario: TESOURA!   x   Maquina: PAPEL!')
+    print('Usuario VENCEU!')
+elif jogada_maquina == 3 and jogada_usuario == 1:
+    print('Usuario: PEDRA!   x   Maquina: TESOURA!')
+    print('Usuario VENCEU!')
+elif jogada_usuario == 1 and jogada_maquina == 2:
+    print('Usuario: PEDRA!   x   Maquina: PAPEL!')
+    print('Usuario PERDEU!')
+elif jogada_usuario == 2 and jogada_maquina == 3:
+    print('Usuario: PAPEL!   x   Maquina: TESOURA!')
+    print('Usuario PERDEU!')
+elif jogada_usuario == 3 and jogada_maquina == 1:
+    print('Usuario: TESOURA!   x   Maquina: PEDRA!')
+    print('Usuario PERDEU!')
+
+
