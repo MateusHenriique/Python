da54facbb0be056f0bae0ee7eb3338bf127b7823
MateuSh5453 <mateush5453@gmail.com>
2024-06-18 21:34:25 -0300

    Atualizei o exercício

--- Python 3 - CursoEmVideo/ExerciciosPhyton/EX045.py
+++ Python 3 - CursoEmVideo/ExerciciosPhyton/EX045.py
@@ -10,27 +10,28 @@ print('[ 2 ] Papel')
 print('[ 3 ] Tesoura')
 jogada_usuario = int(input('Qual será a sua escolha: '))
 print('-='*20)
-print('{:^40}'.format('Carregando...'))
+print('{:^40}'.format('JO'))
+sleep(1)
+print('{:^40}'.format('KEM')) 
+sleep(1)
+print('{:^40}'.format('PO!!'))
 print('-='*20)
-sleep(5)
 jogada_maquina = random.randint(1, 3)
 if jogada_maquina == 1 and jogada_usuario == 2:
     print('Usuario: PAPEL!   x   Maquina: PEDRA!')
-    print('Usuario VENCEU!')
+    print('Usuario VENCE!')
 elif jogada_maquina == 2 and jogada_usuario == 3:
     print('Usuario: TESOURA!   x   Maquina: PAPEL!')
-    print('Usuario VENCEU!')
+    print('Usuario VENCE!')
 elif jogada_maquina == 3 and jogada_usuario == 1:
     print('Usuario: PEDRA!   x   Maquina: TESOURA!')
-    print('Usuario VENCEU!')
+    print('Usuario VENCE!')
 elif jogada_usuario == 1 and jogada_maquina == 2:
     print('Usuario: PEDRA!   x   Maquina: PAPEL!')
-    print('Usuario PERDEU!')
+    print('Maquina VENCE!')
 elif jogada_usuario == 2 and jogada_maquina == 3:
     print('Usuario: PAPEL!   x   Maquina: TESOURA!')
-    print('Usuario PERDEU!')
+    print('Maquina VENCE!')
 elif jogada_usuario == 3 and jogada_maquina == 1:
     print('Usuario: TESOURA!   x   Maquina: PEDRA!')
-    print('Usuario PERDEU!')
-
-
+    print('Maquina VENCE!')
