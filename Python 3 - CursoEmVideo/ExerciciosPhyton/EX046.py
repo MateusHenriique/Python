42f176996653104d41524d27a55fd117f6e23fd1
MateuSh5453 <mateush5453@gmail.com>
2024-06-18 22:46:01 -0300

    Finalizei o exercício

---
+++ Python 3 - CursoEmVideo/ExerciciosPhyton/EX046.py
@@ -0,0 +1,13 @@
+print('-=' * 20)
+print('{:^40}'.format('CONTAGEM PARA OS FOGOS!!'))
+print('-=' * 20)
+
+from time import sleep
+
+for c in range(10, 0, -1):
+    print(c)
+    sleep(1)
+    
+print('-=' * 20)
+print('{:^40}'.format('BOOOOMMMMM!!!'))
+print('-=' * 20)
