b78ef8486dd4edcdc450d7ae1141a431a35450c1
MateuSh5453 <mateush5453@gmail.com>
2024-06-21 23:36:23 -0300

    finalizei o exercício

--- Python 3 - CursoEmVideo/ExerciciosPhyton/EX052.py
+++ Python 3 - CursoEmVideo/ExerciciosPhyton/EX052.py
@@ -1,25 +1,23 @@
 if_primo = int(input('Digite um numero inteiro: '))
 divisivel = 0
 
-if if_primo % 1 == 0 and if_primo % if_primo == 0:
-
-    for c in range(1, if_primo + 1):
+for c in range(1, if_primo + 1):
         
         if if_primo % c == 0:
 
-            print('\033[1;31m {}'.format(c), end='')
+            print('\033[1;33m {}'.format(c), end='')
             divisivel = divisivel + 1
 
         else:
 
-            print('\033[1;33m {}'.format(c), end='')
+            print('\033[1;31m {}'.format(c), end='')
 
-    if divisivel < 2: 
+if divisivel < 2: 
         
-        print('\n\033[0;0mO numero {} foi divisivel {} vezes.'.format(if_primo, divisivel))
-        print('\033[0;0m{} é um número primo.'.format(if_primo))
+    print('\n\033[0;0mO numero {} foi divisivel {} vezes.'.format(if_primo, divisivel))
+    print('\033[0;0m{} é um número primo.'.format(if_primo))
 
-    else:
+else:
 
-        print('\n\033[0;0mO numero {} foi divisivel {} vezes.'.format(if_primo, divisivel))
-        print('\033[0;0m{} não é um número primo.'.format(if_primo))
+    print('\n\033[0;0mO numero {} foi divisivel {} vezes.'.format(if_primo, divisivel))
+    print('\033[0;0m{} não é um número primo.'.format(if_primo))
