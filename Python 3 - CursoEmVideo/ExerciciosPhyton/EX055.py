b6ef805fc4e3ccc45090b38bd6ea550276c17851
MateuSh5453 <mateush5453@gmail.com>
2024-06-25 21:28:37 -0300

    Finalizei o exercício

--- Python 3 - CursoEmVideo/ExerciciosPhyton/EX055.py
+++ Python 3 - CursoEmVideo/ExerciciosPhyton/EX055.py
@@ -1,22 +1,31 @@
 maior_peso = 0
-menor_peso = 1000
+menor_peso = 0
 nome_maior = ''
 nome_menor = ''
 
-for c in range(1, 5):
-    
+for c in range(1, 6):
+
     nome = str(input('Qual o nome da {}° pessoa: '.format(c)).strip())
     peso = float (input('Qual o peso da {}° pessoa: '.format(c)))
 
-    if peso > maior_peso:
+    if c == 1:
 
-        nome_maior = nome
-        maior_peso = peso
-
-    elif peso < menor_peso:
-
-        nome_menor = nome
         menor_peso = peso
+        maior_peso = peso
+        nome_menor = nome
+        nome_maior = nome
 
-print('A pessoa com o maior peso foi {} com {}KG.'.format(nome_maior, maior_peso))
-print('A pessoa com o menor peso foi {} com {}KG.'.format(nome_menor, menor_peso))
+    else:
+
+        if peso > maior_peso:
+
+            nome_maior = nome
+            maior_peso = peso
+
+        elif peso < menor_peso:
+
+            nome_menor = nome
+            menor_peso = peso
+
+print('A pessoa com o maior peso foi {} com {:.2f}KG.'.format(nome_maior, maior_peso))
+print('A pessoa com o menor peso foi {} com {:.2f}KG.'.format(nome_menor, menor_peso))
