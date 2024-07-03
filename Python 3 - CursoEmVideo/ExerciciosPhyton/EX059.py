8f7f408343fc343602750fe460cf781e71669862
MateuSh5453 <mateush5453@gmail.com>
2024-07-03 18:16:25 -0300

    Update EX059.py

--- Python 3 - CursoEmVideo/ExerciciosPhyton/EX059.py
+++ Python 3 - CursoEmVideo/ExerciciosPhyton/EX059.py
@@ -1,38 +1,58 @@
-escolha = 0
+from time import sleep
 
-while escolha != 5:
+escolha = 0
+finalizar = 0
+
+while finalizar != 5:
 
     n1 = int(input('Digite o primeiro valor: '))
     n2 = int(input('Digite o segundo valor: '))
-    print('''
-    [1] Somar
-    [2] Multiplicar
-    [3] Maior
-    [4] Novos Numeros
-    [5] Sair do programa
-    ''')
-    escolha = int(input('Qual das opções voce deseja executar: '))  
+    escolha = 0
 
-    if escolha == 1:
+    while escolha != 4:
 
-        print('a soma dos dois numeros digitados é {}'.format(n1 + n2))
+        print('''
+        [1] Somar
+        [2] Multiplicar
+        [3] Maior
+        [4] Novos Numeros
+        [5] Sair do programa
+        ''')
+        escolha = int(input('Qual das opções voce deseja executar: '))  
 
-    elif escolha == 2:
+        if escolha == 1:
 
-        print('a multiplicação os 2 numeros digitados é: '.format(n1 * n2))
+            print('a soma dos dois numeros digitados é {}'.format(n1 + n2))
+            sleep(2)
 
-    elif escolha == 3:
+        elif escolha == 2:
 
-        if n1 > n2:
+            print('a multiplicação os 2 numeros digitados é: {}'.format(n1 * n2))
+            sleep(2)
 
-            print('O maior numero digitado foi {}'.format(n1))
+        elif escolha == 3:
+
+            if n1 > n2:
+
+                print('O maior numero digitado foi {}'.format(n1))
+                sleep(2)
+
+            else:
+
+                print('O maior numero digitado foi {}'.format(n2))
+                sleep(2)
+
+        elif escolha == 4:
+
+            print('Digite os novos numeros:  ')
+        
+        elif escolha == 5:
+
+            escolha = 4
+            finalizar = 5
 
         else:
 
-            print('O maior numero digitado foi {}'.format(n2))
-
-    elif escolha == 4:
-
-        print('Digite os novos numeros:  ')
+            print('Escolha invalida')
 
 print('Programa finalizado!')
