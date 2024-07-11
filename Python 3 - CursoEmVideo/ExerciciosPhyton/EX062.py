89cf6b778f6f5785668e8aca126eedf8015b51d6
MateuSh5453 <mateush5453@gmail.com>
2024-07-10 21:20:15 -0300

    Atualizei o augoritmo

--- Python 3 - CursoEmVideo/ExerciciosPhyton/EX062.py
+++ Python 3 - CursoEmVideo/ExerciciosPhyton/EX062.py
@@ -1,11 +1,12 @@
 primeiro = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a razão da PA: '))
-continua = 1
-vezes = 10
+continua = 10
+vezes = 0
+c = 0
 
 while continua != 0:
 
-    c = 0
+    vezes += continua
     while c < vezes:
 
         print(primeiro, end=' -> ')
@@ -13,7 +14,6 @@ while continua != 0:
         c += 1
 
     print('PAUSA')
-    vezes = int(input('Quantos termos você quer mostrar mais? '))
-    continua = vezes
+    continua = int(input('Quantos termos você quer mostrar mais? '))
 
-print('FIM')    
+print('FIM... No total foram digitados {} termos.'.format(vezes))    
