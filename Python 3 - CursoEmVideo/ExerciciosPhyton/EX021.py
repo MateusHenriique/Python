65db1773e692ed66d4f0361388155bdf509794fb
Mateus Henrique Da Silva <mateush5453@gmail.com>
2024-08-14 14:26:43 -0300

    Update EX021.py

--- Python 3 - CursoEmVideo/ExerciciosPhyton/EX021.py
+++ Python 3 - CursoEmVideo/ExerciciosPhyton/EX021.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.init()
-pygame.mixer.music.load('Python/ExerciciosPhyton/EX021.mp3')
+pygame.mixer.music.load('Python 3 - CursoEmVideo/ExerciciosPhyton/EX021.mp3')
 pygame.mixer.music.play()
 x = input ('parar')
 pygame.mixer.stop(x)
