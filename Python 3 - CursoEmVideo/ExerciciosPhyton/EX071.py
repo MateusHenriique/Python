8c3933ae674b9802bec9bbf527b92c8299e372dd
MateuSh5453 <mateush5453@gmail.com>
2024-08-13 23:04:37 -0300

    Finalizei o exercicio

--- Python 3 - CursoEmVideo/ExerciciosPhyton/EX071.py
+++ Python 3 - CursoEmVideo/ExerciciosPhyton/EX071.py
@@ -1,13 +1,11 @@
-'''crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte 
-ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai 
-informar quantas cédulas de cada valor serão entregues. OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.'''
-
 print('-='*20)
 print(f'{'CAIXA ELETRONICO':^40}')
 print('-='*20)
 
 saldo = 100
 print(f'Saldo Disponivel: R${saldo}')
+tot_50 = tot_20 = tot_10 = tot_1 = 0
+
 
 while True:
     
@@ -21,6 +19,41 @@ while True:
         print('-='*20)
         saque = int(input('Quanto voce deseja sacar: '))
         
+        if saldo - saque < 0:
+            print('-='*20)
+            print('Saldo insuficiente, operacão cancelada.')
+            
+        else:
+            
+            troco = saque
+            
+            while troco > 0:
+            
+                if troco >= 50:
+                    troco -= 50
+                    tot_50 += 1
+                        
+                elif saque >= 20:
+                    troco -= 20 
+                    tot_20 += 1
+                    
+                elif troco >= 10:
+                    troco -= 10
+                    tot_10 += 1
+                    
+                elif troco >= 1: 
+                    troco -= 1
+                    tot_1 += 1
+                    
+                saldo -= saque
+        
+            print('-='*20)            
+            print(f'valor de saque solicitado: R${saque}')
+            print(f'sera sacado {tot_50} notas de R$50, {tot_20} notas de R$20, {tot_10} notas de R$10 e {tot_1} moedas de R$1')     
+              
+        print('-='*20)
+        print(f'Saldo Disponivel: R${saldo}') 
+            
     if opcao == 2:
         print('-='*20)
         deposito = int(input('Quanto você deseja depositar: '))
@@ -31,4 +64,5 @@ while True:
     if opcao == 3:
         break
 
+print('-='*20)
 print('Programa Finalizado!')
